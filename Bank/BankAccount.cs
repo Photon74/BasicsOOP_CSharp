24becba7a079b3491a400f04a364291b823ee2df
Aleksandr <Photon.74@gmail.com>
2021-10-26 19:51:01 +0300

    Добавил событие в класс счета

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -1,9 +1,9 @@
-﻿using System;
-
-namespace Bank
+﻿namespace Bank
 {
+    public delegate void AccountHandler(string message);
     class BankAccount
     {
+        public event AccountHandler Notify;
         private static double Number;
         private double _accountNumber;
         private decimal _accountBalance;
@@ -14,23 +14,21 @@ namespace Bank
             AccountNumber = ++Number;
             AccountBalance = 0;
             AccountType = AccountType.Deposit;
+
         }
 
-        public BankAccount(decimal accountBalance)
+        public BankAccount(decimal accountBalance) : this()
         {
-            AccountNumber = ++Number;
             AccountBalance = accountBalance;
         }
 
-        public BankAccount(AccountType accountType)
+        public BankAccount(AccountType accountType) : this()
         {
-            AccountNumber = ++Number;
             AccountType = accountType;
         }
 
-        public BankAccount(decimal accountBalance, AccountType accountType)
+        public BankAccount(decimal accountBalance, AccountType accountType) : this()
         {
-            AccountNumber = ++Number;
             AccountBalance = accountBalance;
             AccountType = accountType;
         }
@@ -45,8 +43,16 @@ namespace Bank
         /// <param name="amount">Сумма пополнения</param>
         public void Deposit(decimal amount)
         {
-            if (amount <= 0) return;
+            Notify?.Invoke($"Попытка пополнить счет № {AccountNumber} на сумму: {amount:C2}.");
+
+            if (amount <= 0)
+            {
+                Notify?.Invoke($"Сумма пополнения должна быть больше нуля!\n");
+                return;
+            }
+
             AccountBalance += amount;
+            Notify?.Invoke($"На счет № {AccountNumber} поступило: {amount:C2}\nБаланс: {AccountBalance:C2}.\n");
         }
 
         /// <summary>
@@ -55,8 +61,16 @@ namespace Bank
         /// <param name="amount">Сумма списания</param>
         public void Withdraw(decimal amount)
         {
-            if (amount > AccountBalance) return;
+            Notify?.Invoke($"Попытка снять со счета № {AccountNumber} сумму: {amount:C2}.");
+
+            if (amount > AccountBalance)
+            {
+                Notify?.Invoke($"Недостаточно средств для снятия!\nБаланс: {AccountBalance:C2}.\n");
+                return;
+            }
+
             AccountBalance -= amount;
+            Notify?.Invoke($"Со счета № {AccountNumber} снято: {amount:C2}\nОстаток: {AccountBalance:C2}.\n");
         }
     }
 }
--- Bank/Program.cs
+++ Bank/Program.cs
@@ -7,12 +7,16 @@ namespace Bank
     {
         static void Main()
         {
+            Console.OutputEncoding = Encoding.UTF8; // Иначе вопросики выводил вместо знака рубля.
+
             BankAccount bankAccount = new();
+            bankAccount.Notify += message => Console.WriteLine(message);
+
             BankAccount bankAccount1 = new(accountBalance: 500);
             BankAccount bankAccount2 = new(accountType: AccountType.Credit);
             BankAccount bankAccount3 = new(accountBalance: 358, accountType: AccountType.Deposit);
 
-            Console.OutputEncoding = Encoding.UTF8; // Иначе вопросики выводил вместо знака рубля.
+
             Console.WriteLine(
                 $"Номер счета: {bankAccount.AccountNumber}\n" +
                 $"Баланс счета: {bankAccount.AccountBalance:C}\n" +
@@ -36,6 +40,11 @@ namespace Bank
                 $"Баланс счета: {bankAccount3.AccountBalance:C}\n" +
                 $"Тип счета: {bankAccount3.AccountType}\n"
                 );
+
+            bankAccount.Deposit(0);
+            bankAccount.Deposit(100);
+            bankAccount.Withdraw(200);
+            bankAccount.Withdraw(50);
         }
     }
 }
