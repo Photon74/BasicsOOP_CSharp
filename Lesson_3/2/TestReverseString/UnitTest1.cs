2421ac3b2110f9e7dbf867dbd814871700da02f9
Aleksandr <Photon.74@gmail.com>
2021-10-30 17:32:43 +0300

    ReverseString

---
+++ Lesson_3/2/TestReverseString/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestReverseString
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase  ]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
