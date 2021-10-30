cf621f3a2ded008f6db2b1539304731e7051cc8e
Aleksandr <Photon.74@gmail.com>
2021-10-31 01:06:09 +0300

    EmailFinder готов

---
+++ Lesson_3/3/WorkingWithStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WorkingWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceFile = "test.txt";
+            var destinationFile = "result.txt";
+
+            EmailFinder emailFinder = new();
+
+            emailFinder.Find(sourceFile, destinationFile);
+        }
+    }
+}
