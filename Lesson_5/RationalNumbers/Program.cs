60cd6485455b6ab4d525d524fadbd20aed6996ee
Aleksandr <Photon.74@gmail.com>
2021-11-06 23:01:39 +0300

    RationalNumbers

---
+++ Lesson_5/RationalNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace RationalNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rationals rational1 = new(1, 3);
+            Rationals rational2 = new(1, 2);
+            Rationals rational3 = rational1 / rational2;
+
+            System.Console.WriteLine(rational3);
+        }
+    }
+}
